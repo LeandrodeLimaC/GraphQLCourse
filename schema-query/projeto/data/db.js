74fe1b949ba0a79fac2f1bbd22ceb870bf0debc0
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-04 03:31:55 -0300

    Example of enum

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -14,19 +14,22 @@ const usuarios = [{
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'ATIVO'
 }, {
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'INATIVO'
 }, {
     id: 3,
     nome: 'Daniela Smith',
     email: 'danismi@uemail.com',
     idade: 24,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'BLOQUEADO'
 }]
 
 module.exports = { usuarios, perfis, permissoes }
