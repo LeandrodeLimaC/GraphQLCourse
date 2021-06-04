96936066966bdda249b19a61e763263a5c7855ee
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-04 14:59:17 -0300

    Modify user mutation

--- mutations/projeto/resolvers/Mutation.js
+++ mutations/projeto/resolvers/Mutation.js
@@ -25,5 +25,17 @@ module.exports = {
 
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos[0] : null
+    },
+
+    alterarUsuario(_, args) {
+        const i = usuarios.findIndex(usuario => usuario.id === args.id)
+
+        const usuario = {
+            ...usuarios[i],
+            ...args
+        }
+
+        usuarios.splice(i, 1, usuario)
+        return usuarios[i]
     }
 }
