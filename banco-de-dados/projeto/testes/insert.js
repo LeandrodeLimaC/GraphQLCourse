7c1ed19ee57ea4a8d361ac174fabc622e308b562
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-08 23:39:54 -0300

    novo Perfil visitante example

---
+++ banco-de-dados/projeto/testes/insert.js
@@ -0,0 +1,11 @@
+const db = require('../config/db')
+
+// const novoPerfil = {
+//     nome: 'visitante',
+//     rotulo: 'Visitante'
+// }
+
+// db('perfis').insert(novoPerfil)
+//     .then(console.log)
+//     .catch(err => console.log(err.sqlMessage))
+//     .finally(() => db.destroy())
